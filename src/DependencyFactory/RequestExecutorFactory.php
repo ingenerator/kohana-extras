6668e67651bc3403050d1eb7c6edf17bf5ddf434
Andrew Coulton <andrew@ingenerator.com>
2018-03-08 01:22:22 +0000

    Add dependency definition helper for controller services

--- src/DependencyFactory/RequestExecutorFactory.php
+++ src/DependencyFactory/RequestExecutorFactory.php
@@ -28,4 +28,39 @@ class RequestExecutorFactory
             ]
         ];
     }
+
+    /**
+     * Helper to generate dependency definitions for a simple list of controller class => dependencies
+     *
+     * Produces the definitions with the keys / in the format the request executor expects to find
+     * them.
+     *
+     * eg:
+     *   _include => [
+     *     RequestExecutorFactory::controllerDefinitions(
+     *       [
+     *         Some\Custom\Controller::class => ['%kohana.log%', '@some.config@'],
+     *       ]
+     *     )
+     *   ]
+     *
+     * @param array $controllers
+     *
+     * @return array
+     */
+    public static function controllerDefinitions(array $controllers)
+    {
+        $definitions = ['controller' => []];
+        foreach ($controllers as $controller_class => $controller_args) {
+            $key                             = '\\'.trim($controller_class, '\\');
+            $definitions['controller'][$key] = [
+                '_settings' => [
+                    'class'     => $controller_class,
+                    'arguments' => $controller_args,
+                ],
+            ];
+        }
+
+        return $definitions;
+    }
 }
